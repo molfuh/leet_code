1e8f964cf83e83aff28ed75588f84df95966c375
Molly Fuhrman <mollyfuhrman@Mollys-Air.lan1>
2021-03-12 22:48:59 -0800

    First implementation of selection sort.

---
+++ selectionSort/selectionSort.js
@@ -0,0 +1,21 @@
+function selectionSort(arr) {
+  for (let j = 0; j < arr.length; j++) {
+      let smallest = j;
+      for (let i = j + 1; i < arr.length; i++) {
+          smallest = arr[smallest] > arr[i] ? i : smallest;
+        }
+      let temp = arr[j];
+      arr[j] = arr[smallest];
+      arr[smallest] = temp;
+  }
+  return arr;
+}
+
+//O(n^2) time complexity
+//O(n) space complexity
+
+(() => {
+                                        // i  
+    console.log(selectionSort([5, 4, 3, 2, 1]));
+                                        //    s
+})();
