82c6677e536160ecfb78bbec2549e672fc58b149
Molly Fuhrman <mollyfuhrman@gmail.com>
2021-03-30 10:54:28 -0700

    First comments to outline plan.

---
+++ twoSum/twoSum.js
@@ -0,0 +1,21 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+ var twoSum = function(nums, target) {
+  //two pointers, starting at first and second indices
+  //while first index is less than second index
+    //check sum of both elements with pointers
+    //if sum equals target,
+      //return both nums
+    //otherwise
+      //
+};
+
+//I: arr of ints and int target
+//O: indices of two numbers so that they add up to target
+//C: will not have multiple solutions
+//E:
+// [3,2,4], 6
+// returns [2, 4]
